ab074260c58ac7d450cc3e457c075244fb692ae3
Christian Fratta <christian.fratta@gmail.com>
2024-08-31 19:29:48 +0200

    Separate schedules & time limits

--- whip/storage/PersistenceManaging.swift
+++ whip/storage/PersistenceManaging.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 protocol PersistenceManaging {
-    func saveTimeLimitRules(_ rules: [String: TimeInterval]) -> Result<Void, Error>
-    func loadTimeLimitRules() -> Result<[String: TimeInterval], Error>
+    func saveTimeLimitRules(_ rules: [String: TimeLimit]) -> Result<Void, Error>
+    func loadTimeLimitRules() -> Result<[String: TimeLimit], Error>
     
     func saveUsageData(_ data: [String: [String: TimeInterval]]) -> Result<Void, Error>
     func loadUsageData() -> Result<[String: [String: TimeInterval]], Error>
